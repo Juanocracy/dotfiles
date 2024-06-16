7e683c67eb9e19e5bd3ab83c0917a46f142bb4bf
Juan Mendoza <camilop420@hotmail.com>
2024-06-16 16:53:37 -0500

    InstallArch.sh chaged for more legibility

--- .config/installPackagesArch.sh
+++ .config/installPackagesArch.sh
@@ -13,50 +13,81 @@ if ! command -v yay &>/dev/null; then
 	cd yay/
 	makepkg -si
 	cd ..
-  rm -rf yay
+	rm -rf yay
 else
 	echo "Yay ya está instalado"
 fi
 
-#Paquetes necesarios desde una instalación barebone
-# hyprlan kitty sddm pipewire
+# Paquetes necesarios para hyprland desde una instalación barebone
+# hyprland kitty sddm pipewire
 
-# Paquetes desde repositorios oficiales
-sudo pacman -Syyu --noconfirm swaylock btop jdk-openjdk python noto-fonts dolphin discord obsidian steam blueman git nvim bluez bluez-tools bluez-utils blueman pavucontrol swaync wofi waybar unzip cliphist starship ripgrep fd ttf-cascadia-code
+# Instalar paquetes desde repositorios oficiales
+official_packages=(
+	swaylock
+	btop
+	jdk-openjdk
+	python
+	noto-fonts
+	dolphin
+	discord
+	obsidian
+	steam
+	blueman
+	git
+	nvim
+	bluez
+	bluez-tools
+	bluez-utils
+	pavucontrol
+	swaync
+	wofi
+	waybar
+	unzip
+	cliphist
+	starship
+	ripgrep
+	fd
+	ttf-cascadia-code
+)
 
-#Revisar el estado del bluetooth
-systemctl status bluetooth.service
-#Activar el servicio
-systemctl start bluetooth.service
+sudo pacman -Syyu --noconfirm "${official_packages[@]}"
 
-#Instalar LazyVim
-git clone https://github.com/LazyVim/Starter ~/.config/nvim rm -rf ~/.config/nvim/.git
+# Revisar el estado del bluetooth y activar el servicio si no está activo
+if ! systemctl is-active --quiet bluetooth.service; then
+	echo "Activando el servicio Bluetooth..."
+	sudo systemctl start bluetooth.service
+fi
 
-# Paquetes desde AUR con yay
-yay_packages= (
-  onlyoffice-bin
-  microsoft-edge-stable-bin
-  nerd-fonts-git
-  ttf-ms-fonts
-  lavat-git
-  cava-git
-  ttf-font-awesome
-  awesome-terminal-fonts
-  noto-fonts-emoji
-  noto-fonts-cjk
-  noto-fonts-extra
+# Instalar LazyVim
+git clone https://github.com/LazyVim/Starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
 
-  vmware-workstation
-  swww
-  wlogout
-  ttf-cascadia-code
-  visual-studio-code-bin
-  intellij-idea-ultimate-edition
-  spotify-adblock-git
-  )
+# Instalar paquetes desde AUR con yay
+aur_packages=(
+	onlyoffice-bin
+	microsoft-edge-stable-bin
+	nerd-fonts-git
+	ttf-ms-fonts
+	lavat-git
+	cava-git
+	ttf-font-awesome
+	awesome-terminal-fonts
+	noto-fonts-emoji
+	noto-fonts-cjk
+	noto-fonts-extra
+	vmware-workstation
+	swww
+	wlogout
+	visual-studio-code-bin
+	intellij-idea-ultimate-edition
+	spotify-adblock-git
+)
 
-  for pkg in "${yay_packages[@]}"; do yay -Syyu --noconfirm "$pkg" done
+for pkg in "${aur_packages[@]}"; do
+	yay -Syyu --noconfirm "$pkg"
+done
 
-  # Habilitar servicios
-  sudo systemctl enable sddm.service bluetooth.service docker.service
-  echo "Instalación y configuración completada."
+# Habilitar servicios
+sudo systemctl enable sddm.service bluetooth.service docker.service
+
+echo "Instalación y configuración completada."
