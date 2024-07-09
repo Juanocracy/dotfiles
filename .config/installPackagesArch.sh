581b202fb8782cea1e6c6a9c31d8290621909025
Juan <camilop420@hotmail.com>
2024-07-09 05:25:44 -0500

    Added waveform and .oh-my-zsh

--- .config/installPackagesArch.sh
+++ .config/installPackagesArch.sh
@@ -8,14 +8,14 @@ sudo pacman -S --needed base-devel
 
 # Verificar si yay está instalado
 if ! command -v yay &>/dev/null; then
-	echo "Instalando yay..."
-	git clone https://aur.archlinux.org/yay
-	cd yay/
-	makepkg -si
-	cd ..
-	rm -rf yay
+  echo "Instalando yay..."
+  git clone https://aur.archlinux.org/yay
+  cd yay/
+  makepkg -si
+  cd ..
+  rm -rf yay
 else
-	echo "Yay ya está instalado"
+  echo "Yay ya está instalado"
 fi
 
 # Paquetes necesarios para hyprland desde una instalación barebone
@@ -23,59 +23,60 @@ fi
 
 # Instalar paquetes desde repositorios oficiales
 official_packages=(
-	fastfetch
-	swaylock
-	btop
-	jdk-openjdk
-	python
-	noto-fonts
-	zsh # Command terminal is fundamental to configure the shell and the framework with the promp.
-	zellij
-	thunar #dolphin
-	thunderbird
-	discord
-	obsidian
-	#steam
-	blueman
-	git
-	neovim
-	bluez
-	bluez-tools
-	bluez-utils
-	pavucontrol
-	swaync
-	wofi
-	waybar
-	unzip
-	cliphist
-	starship
-	ripgrep
-	fd
-	ttf-cascadia-code
-	dotnet-sdk
-	packagekit-qt6 # For installing apps through Discover on plasma
+  fastfetch
+  swaylock
+  btop
+  jdk-openjdk
+  python
+  noto-fonts
+  zsh # Command terminal is fundamental to configure the shell and the framework with the promp.
+  zellij
+  thunar #dolphin
+  thunderbird
+  discord
+  obsidian
+  #steam #lib32-amdvlk for amd processor.
+  blueman
+  git
+  neovim
+  bluez
+  bluez-tools
+  bluez-utils
+  pavucontrol
+  swaync
+  wofi
+  waybar
+  unzip
+  cliphist
+  starship
+  ripgrep
+  fd
+  ttf-cascadia-code
+  dotnet-sdk
+  packagekit-qt6 # For installing apps through Discover on plasma
+  os-prober      #For grub detection.
 
-	#Media Software
-	obs-studio
-	gimp
-	audacity
-	darktable
+  #Media Software
+  obs-studio
+  gimp
+  audacity
+  darktable
 )
 
 # Iterar sobre la lista de paquetes e intentar instalar cada uno
 for pkg in "${official_packages[@]}"; do
-	echo "Instalando $pkg..."
-	sudo pacman -S --noconfirm "$pkg"
-	if [ $? -ne 0 ]; then
-		echo "Error al instalar $pkg. Abortando."
-		exit 1
-	fi
+  echo "Instalando $pkg..."
+  sudo pacman -S --noconfirm "$pkg"
+  if [ $? -ne 0 ]; then
+    echo "Error al instalar $pkg. Abortando."
+    exit 1
+  fi
 done
 
 # Revisar el estado del bluetooth y activar el servicio si no está activo
 if ! systemctl is-active --quiet bluetooth.service; then
-	echo "Activando el servicio Bluetooth..."
-	sudo systemctl start bluetooth.service
+  echo "Activando el servicio Bluetooth..."
+  sudo systemctl start bluetooth.service
 fi
 
 # Instalar LazyVim
@@ -84,34 +85,35 @@ rm -rf ~/.config/nvim/.git
 
 # Instalar paquetes desde AUR con yay
 aur_packages=(
-	onlyoffice-bin
-	microsoft-edge-stable-bin
-	#nerd-fonts-git
-	ttf-ms-fonts
-	lavat-git
-	cava-git
-	ttf-font-awesome
-	awesome-terminal-fonts
-	noto-fonts-emoji
-	emote
-	noto-fonts-cjk
-	noto-fonts-extra
-	vmware-workstation
-	swww
-	wlogout
-	visual-studio-code-bin
-	intellij-idea-ultimate-edition
-	spotify-adblock-git
+  onlyoffice-bin
+  microsoft-edge-stable-bin
+  #nerd-fonts-git
+  ttf-ms-fonts
+  lavat-git
+  cava-git
+  ttf-font-awesome
+  awesome-terminal-fonts
+  noto-fonts-emoji
+  emote
+  noto-fonts-cjk
+  noto-fonts-extra
+  vmware-workstation
+  swww
+  wlogout
+  visual-studio-code-bin
+  intellij-idea-ultimate-edition
+  spotify-adblock-git
 
-	# .Net
-	rider
+  # .Net
+  rider
 
-	#Media Software
-	davinci-resolve
+  #Media Software
+  davinci-resolve
+  tracktion-waveform
 )
 
 for pkg in "${aur_packages[@]}"; do
-	yay -Syyu --noconfirm "$pkg"
+  yay -Syyu --noconfirm "$pkg"
 done
 
 # Habilitar servicios
