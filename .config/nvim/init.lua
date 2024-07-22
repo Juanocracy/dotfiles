33a7b788122a47721e8a90cd2e58c50e72438f01
Juan <camilop420@hotmail.com>
2024-07-22 03:32:19 -0500

    nvim and oh-my-zsh files modified

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+return {
+  "tokyonight.nvim",
+  lazy = true,
+  priority = 1000,
+  opts = function()
+    return {
+      transparent = true,
+    }
+  end,
+}
