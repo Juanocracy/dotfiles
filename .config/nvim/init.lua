800c34ecac40ced4d4d874b0002eaa6df46a76cb
Juanocracy <camilop420@hotmail.com>
2025-01-19 12:16:53 -0500

    Change lazy.lua adding just catppuccin at line 48, and commented the tokyo return function i guess, and added return catppuccin in the init.lua for change the theme.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,13 +19,65 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+-- Configuración del tema Catppuccin
 return {
-  "tokyonight.nvim",
-  lazy = true,
-  priority = 1000,
-  opts = function()
-    return {
-      transparent = true,
-    }
-  end,
+  "catppuccin/nvim",
+  name = "catppuccin",
+  opts = {
+    transparent_background = true, -- Fondo transparente
+    term_colors = true,
+    styles = {
+      comments = "italic",
+      keywords = "bold",
+    },
+    integrations = {
+      aerial = true,
+      alpha = true,
+      cmp = true,
+      dashboard = true,
+      flash = true,
+      fzf = true,
+      grug_far = true,
+      gitsigns = true,
+      headlines = true,
+      illuminate = true,
+      indent_blankline = { enabled = true },
+      leap = true,
+      lsp_trouble = true,
+      mason = true,
+      markdown = true,
+      mini = true,
+      native_lsp = {
+        enabled = true,
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
+      navic = { enabled = true, custom_bg = "lualine" },
+      neotest = true,
+      neotree = true,
+      noice = true,
+      notify = true,
+      semantic_tokens = true,
+      snacks = true,
+      telescope = true,
+      treesitter = true,
+      treesitter_context = true,
+      which_key = true,
+    },
+  },
+  vim.cmd.colorscheme("catppuccin"),
 }
+--return {
+--  "tokyonight.nvim",
+--  lazy = true,
+--  priority = 1000,
+--  opts = function()
+--    return {
+--      transparent = true,
+--    }
+--  end,
+--}
